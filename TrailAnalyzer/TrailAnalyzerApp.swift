02fc5b0f283c09dc642b57d97ee8d81ee144ab33
Mansoor <mansoor@Mansoors-MacBook-Air.local>
2025-05-29 02:06:42 +0500

    Initial commit

---
+++ TrailAnalyzer/TrailAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailAnalyzerApp.swift
+//  TrailAnalyzer
+//
+//  Created by Mansoor on 29/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct TrailAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
